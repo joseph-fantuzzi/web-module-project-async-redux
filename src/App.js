33d07747c32ea644648b42a9f5e35ed15dc91b31
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-23 16:21:46 -0400

    setup the project and added some jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./App.css";
+
+//https://api.zippopotam.us/us/33162
 
 function App() {
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="App">
-      Async Redux Project
+    <div>
+      <h1>Zip Code Information</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="search">Search a Zip Code</label>
+        <input type="text" id="search" value="" onChange={changeHandler} />
+        <button>Search</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default connect(null, {})(App);
